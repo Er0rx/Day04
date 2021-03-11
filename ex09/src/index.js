a7fcce70c1da00c65122e038fcba9b97e077e2de
Er0rx <xer0rx123@gmail.com>
2021-03-11 22:26:15 +0100

    The end

---
+++ ex09/src/index.js
@@ -0,0 +1,38 @@
+function main() {
+    // Only change code below this line
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
+    myX += 3;
+    myY += 17;
+    myZ -= 20;
+    // Only change code above this line
+    return {
+        myX,
+        myY,
+        myZ
+    }
+}
+
+console.log(main());
+module.exports = main;
+
+
+function main() {
+    // Only change code below this line
+    var firstNum = 6;
+    var secondNum = 25;
+    var thirdNum = 5.2;
+    firstNum *= 9;
+    secondNum /= 5;
+    thirdNum *= 10;
+    // Only change code above this line
+    return {
+        firstNum,
+        secondNum,
+        thirdNum
+    };
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "Mile";
+    var myLastName = "Milovanovic";
+    var myHomeTown = "Mars";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }
+}
+
+console.log(main());
+module.exports = main;
